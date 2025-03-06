1d9127f560b006dcef0658bf15b543006d7807f5
Danila Valko <danila.valko@proton.me>
2025-03-06 21:22:36 +0100

    Update __init__.py

--- map_uk/__init__.py
+++ map_uk/__init__.py
@@ -3,7 +3,12 @@ from __future__ import annotations  # noqa: D104
 from map_uk.map.choropleth import ChoroplethMapUK
 from map_uk.map.map import MapUK
 
+class Map(map_uk.ChoroplethMapUK):
+    def __init__(self, **kwargs):
+        super().__init__(data_dir=map_uk.constants.TOP_FOLDER / "map_uk", **kwargs)
+
 __all__ = (
     "ChoroplethMapUK",
     "MapUK",
+    "Map",
 )
